8b2486905baebe7244e17ff51b5c7df44b9c187b
Kaewan Gardi <kgardi@cvent.com>
2022-10-14 16:19:15 -0700

    Initial Commit

---
+++ ARF/ARFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFApp.swift
+//  ARF
+//
+//  Created by Kaewan Gardi on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ARFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
